1334f8d35b413942034dc0cb0fa6925012d17036
Jado08 <julieanndingalorcine@gmail.com>
2024-09-23 13:15:41 +0800

    Add project files.

---
+++ DataStructures/Person.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace DataStructures
+{
+    internal class Person //connected to case #1
+    {
+        public string name;
+        public int age;
+        public string address;
+        public Dog dog; 
+        public Person(string name, int age, string address)
+        {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+        public void selfIntroduce()
+        {
+            Console.WriteLine("I am " + name + ". My age is " + age + " years old. I lived in " + address + ".");
+            if (dog != null)
+            {
+                Console.WriteLine("I have a " + dog.color + " dog and the name of the dog is " + dog.dogname + ".");
+                Console.WriteLine(dog.isBarking
+                    ? "The dog is barking while sitting down: " + dog.isBarking
+                    : "The dog is barking while standing up: " + dog.isBarking);
+            }
+        }
+    }
+    class Dog //connected to case # 1
+    {
+        public string dogname;
+        public string color;
+        public bool isBarking;
+        public Person personOwned;
+
+        public Dog(string dogname, string color, bool isBarking)
+        {
+            this.dogname = dogname;
+            this.color = color;
+            this.isBarking = isBarking;
+        }
+        public void barkingWhileSittingDown()
+        {
+            this.isBarking = true;
+        }
+        public void barkingWhileStandingUp()
+        {
+            this.isBarking = false;
+        }
+    }
+    class Node //connected to case # 2
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+        public int countNodes(Node head)
+        {
+            int count = 1;
+            Node current = head;
+            while (current.next != null)
+            {
+                current = current.next;
+                count += 1;
+            }
+            return count;
+        } 
+        public void printlist(Node head)
+        {
+            Node current = head;
+            while (current != null)
+            {
+                Console.WriteLine(current.data + " ");
+                current = current.next;
+            }
+            Console.WriteLine();
+        }
+    }
+
+
+
+
+}
---
+++ DataStructures/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a Topic for the Data Structures : ");
+            Console.WriteLine("1. Introduction to Classes and Objects (Part 1 and Part 2)");
+            Console.WriteLine("2. Linked List");
+            Console.WriteLine("3. Stack and Queues");
+
+            Console.Write("\nEnter your choice : ");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("\n***Introduction to Classes and Objects (Part 1 and Part 2)***\n");
+                    Person ce = new Person("Emmanuel", 24, "Matina Aplaya");
+                    Dog l = new Dog("Loki", "Black", false);
+                    ce.dog = l;
+                    l.barkingWhileStandingUp();
+                    ce.selfIntroduce();
+
+                    Person ra = new Person("Russell Ann", 24, "Catalunan Pequino");
+                    Dog m = new Dog("Moshi", "White", true);
+                    ra.dog = m;
+                    m.barkingWhileSittingDown();
+                    ra.selfIntroduce();
+                    break;
+                case 2:
+                    Console.WriteLine("\n***Linked List Operations***\n");
+
+                    Node head = new Node(2);
+                    Node second = new Node(4);
+                    Node third = new Node(6);
+                    Node fourth = new Node(10);
+
+                    head.next = second;
+                    second.next = third;
+                    third.next = fourth;
+
+                    Console.WriteLine("The linked list contains:");
+                    head.printlist(head);
+
+                    int nodeCount = head.countNodes(head);
+                    Console.WriteLine("Number of nodes in the linked list: " + nodeCount);
+                    break;
+                case 3:
+
+
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    break;
+            }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
