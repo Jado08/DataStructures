146591f64559a994e85335733fba79b8dc630f42
Jado08 <julieanndingalorcine@gmail.com>
2024-10-01 15:11:27 +0800

    Greedy Algorithm

---
+++ DataStructures/GreedyAlgo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    internal class GreedyAlgo
+    {
+        public static List<int> PaperLeafCount(int[] paperLeaf, int pcs)
+        {
+            List<int> result = new List<int>();
+
+            foreach(var leaf in paperLeaf)
+            {
+                while(pcs >= leaf)
+                {
+                    pcs -= leaf;
+                    result.Add(leaf);
+                }
+            }
+            return result;
+        }
+        public static void PracticeGreedyAlgo()
+        {
+             int[] paperLeaf = { 2, 3, 1, 5 };
+             int pcs = 15;
+
+             List<int> totalLeaf = PaperLeafCount(paperLeaf, pcs);
+
+             Console.Write($"The total of paper leaf to make {pcs} is {totalLeaf.Count}");
+             Console.WriteLine("\nPaper Leaf used :");
+             foreach(var leaf in totalLeaf)
+             {
+                 Console.WriteLine($"{leaf} pc");
+             }
+        }
+    }
+
+    
+}
